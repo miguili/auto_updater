32e9e0db9f6bcea968cd3887c3bf0ee1e9d3ca1d
1200172 <1200172@isep.ipp.pt>
2021-02-08 18:47:59 +0000

    Version 1.0.6 update

--- main.js
+++ main.js
@@ -3,49 +3,51 @@ const { autoUpdater } = require('electron-updater');
 
 let mainWindow;
 
-function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-        },
-    });
-    mainWindow.loadFile('index.html');
-    mainWindow.on('closed', function () {
-        mainWindow = null;
-    });
-    mainWindow.once('ready-to-show', () => {
-        autoUpdater.checkForUpdatesAndNotify();
-    });
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadFile('index.html');
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+  mainWindow.once('ready-to-show', () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
 }
 
 app.on('ready', () => {
-    createWindow();
+  createWindow();
 });
 
 app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', function () {
-    if (mainWindow === null) {
-        createWindow();
-    }
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
 
 ipcMain.on('app_version', (event) => {
-    event.sender.send('app_version', { version: app.getVersion() });
+  event.sender.send('app_version', { version: app.getVersion() });
 });
 
 autoUpdater.on('update-available', () => {
-    mainWindow.webContents.send('update_available');
+  mainWindow.webContents.send('update_available');
 });
+
 autoUpdater.on('update-downloaded', () => {
-    mainWindow.webContents.send('update_downloaded');
+  mainWindow.webContents.send('update_downloaded');
 });
+
 ipcMain.on('restart_app', () => {
-    autoUpdater.quitAndInstall();
+  autoUpdater.quitAndInstall();
 });
